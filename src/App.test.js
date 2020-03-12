1ebd69a45dceecbdacb0af9604527b8d82e2b85e
Tyler Alsop <tylerjalsop@gmail.com>
2020-03-11 18:12:04 -0600

    Created App.test.js and imported App, React and some functions from the react testing library.

---
+++ src/App.test.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { render, fireEvent, waitForElement, wait } from '@testing-library/react';
+import App from './App'
+
--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -2,12 +2,25 @@ import React from "react";
 import axios from "axios";
 
 export const fetchShow = () => {
-    return (
-        axios
-            .get("https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes")
-            .then(res => {
+    
+    return axios.get("https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes")
+        .then(res => {
+            console.log(res)
                 return res.data
-            })
-    );
+        });
 
 };
+
+// useEffect(() => {
+//     const fetchShow = () => {
+//       axios
+//         .get(
+//           "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+//         )
+//         .then(res => {
+//           setShow(res.data);
+//           setSeasons(formatSeasons(res.data._embedded.episodes));
+//         });
+//     };
+//     fetchShow();
+//   }, []);
